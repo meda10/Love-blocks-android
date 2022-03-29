3b6b797a7cb463d79f5e746216b40c1cab7bf990
meda10 <32466683+meda10@users.noreply.github.com>
2022-03-29 14:48:36 +0200

    Add Dialogs

--- app/src/main/java/blocks/love/utils/utils.kt
+++ app/src/main/java/blocks/love/utils/utils.kt
@@ -1,5 +1,10 @@
 package blocks.love.utils
 
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
@@ -23,4 +28,40 @@ fun View.showSnackbar(msg: String, length: Int, actionMessage: CharSequence?, ac
             action(this)
         }.show()
     }
+}
+
+fun View.showDialog(msgId: Int, titleId: Int, context: Context) {
+    showDialog(context.getString(msgId), context.getString(titleId), context)
+}
+
+fun View.showDialog(msg: String, title: String, context: Context) {
+    val dialogBuilder = AlertDialog.Builder(context)
+
+    dialogBuilder.setMessage(msg)
+        .setCancelable(true)
+//        .setPositiveButton("Install") { dialog, _ -> dialog.dismiss() }
+        .setNegativeButton("Close") { dialog, id -> dialog.cancel() }
+
+    val alert = dialogBuilder.create()
+    alert.setTitle(title)
+    alert.show()
+}
+
+fun View.showDialogInstall(msgId: Int, titleId: Int, context: Context, packageName: String) {
+    val dialogBuilder = AlertDialog.Builder(context)
+
+    dialogBuilder.setMessage(context.getString(msgId))
+        .setCancelable(true)
+        .setPositiveButton("Install") { _, _ ->
+            try {
+                context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+            } catch (e: ActivityNotFoundException) {
+                context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName")))
+            }
+        }
+        .setNegativeButton("Close") { dialog, _ -> dialog.cancel() }
+
+    val alert = dialogBuilder.create()
+    alert.setTitle(context.getString(titleId))
+    alert.show()
 }
